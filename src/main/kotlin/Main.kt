fe9cc3f899aa18948dc26006128e2de108bc0146
Elizabeth Njoroge <njorogewambuielizabeth@gmail.com>
2023-02-02 10:08:53 +0300

    created new file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var name = "Sparksnjoroge"
+    var  age = 22
+    var phoneNumber = "0742324744"
+    var weightInKg = 50
+    val citizenshipKenyan = true
+
+    println ( citizenshipKenyan)
+
+
+}
